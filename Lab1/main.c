94da3564cfd2087ae37db06fdc492716958b968c
Cameron Swinoga <cameron@DESKTOP-TAFEGDQ.Home>
2017-01-05 19:44:31 -0500

    Finished assignment

--- Lab1/main.c
+++ Lab1/main.c
@@ -3,20 +3,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-// user -defined  signal  handler  for  alarm.
-void  alarm_handler(int  signo) {
-	if (signo  ==  SIGALRM)
-		printf (" alarm  goes  off\n");
+void signalHandler(int  signo) {
+	if (signo  ==  SIGALRM) {
+		printf ("Alarm\n");
+		alarm (2); // Set up the next alarm
+		return;
+	}
+
+	if (signo  ==  SIGINT) {
+		printf ("CTRL+C pressed!\n");
+		return;
+	}
+
+	if (signo  ==  SIGTSTP) {
+		printf ("CTRL+Z pressed!\n");
+		exit(0);
+	}
 }
 
-int  main(void) {
-	if (signal(SIGALRM, alarm_handler) ==  SIG_ERR) { //  register  the  signal  handler
-		printf (" failed  to  register  alarm  handler .");
+int main(void) {
+	if (signal(SIGALRM, signalHandler) ==  SIG_ERR ||
+	        signal(SIGINT, signalHandler) ==  SIG_ERR ||
+	        signal(SIGTSTP, signalHandler) ==  SIG_ERR) { //  register  the  signal  handler
+		printf ("Failed to register an alarm handler");
 		exit (1);
 	}
 
-	alarm (3);                 // set  alarm  to fire in 3 seconds.
+	alarm (2); // Fire off the initial alarm
 
 	while (1)
-		sleep (10); // wait  until  alarm  goes  off
+		sleep (10); // Wait for a signal
 }
