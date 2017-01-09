61d994b2f580458d34ee49b01b625455b3ce0d7f
Cameron Swinoga <cameron@debainVM64>
2017-01-09 12:30:31 -0500

    fork() example added

---
+++ Lab2/question2.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+
+int main () {
+	if ( fork() == 0 )
+		printf ("This is the child process\n");
+
+	else
+		printf ("I am the parent process\n");
+
+	printf ("Both parent and child reach here.\r\n");
+	return 0;
+}
