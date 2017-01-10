ff39bd854818a9c87a7f0eefd371871121630d7a
Cameron Swinoga <cameron@debainVM64>
2017-01-10 11:30:47 -0500

    Lab 2 completed

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,35 +1,60 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 int main (void) {
-	int fd [2], nbytes;
+	int ctop[2], ptoc[2];
 	pid_t childpid;
-	pipe (fd);
+	pipe(ctop);
+	pipe(ptoc);
 
 	if ((childpid = fork()) == -1) {
-		perror ("Fork error");
-		exit (1);
+		perror("Fork error");
+		exit(1);
 	}
 
-	if (childpid == 0) {
-		close (fd[0]);
-		char string[256];
-		int stringPos = 0;
+	if (childpid == 0) { // Child process
+		close(ctop[0]); // Close read pipe
+		close(ptoc[1]); // Close write pipe
+		char input[256] = {0,};
+		char readbuffer[256] = {0,};
 
-		while (strstr(string, "-1") == 0) {
-			string[stringPos] = getc(stdin);
-			char sendStr[2] = {string[stringPos], 0};
-			stringPos++;
-			write (fd[1], sendStr, (strlen(sendStr) + 1));
+		while (strstr(input, "-1") == 0) {
+			scanf("%s", input);
+			write(ctop[1], input, strlen(input) + 1);
 		}
 
-	} else {
-		close (fd[1]);
-		char readbuffer[80];
-		nbytes = read (fd[0], readbuffer, sizeof(readbuffer));
-		printf ("Received string: %s", readbuffer);
+		read(ptoc[0], readbuffer, sizeof(readbuffer));
+		printf("\nSum: %s\r\n", readbuffer);
+		//printf("Exit child\n");
+
+	} else { // Parent process
+		close(ctop[1]); // Close write pipe
+		close(ptoc[0]); // Close read pipe
+		char readbuffer[256] = {0,};
+		char writebuffer[256] = {0,};
+		char *endStr;
+		int nums[256] = {0,};
+		int sum = 0;
+		unsigned int sumPos = 0;
+		read(ctop[0], readbuffer, sizeof(readbuffer));
+
+		while (strcmp(readbuffer, "-1") != 0) {
+			nums[sumPos] = strtol(readbuffer, &endStr, 10);
+			sumPos++;
+			//printf("Received string: %s\n", readbuffer);
+			read(ctop[0], readbuffer, sizeof(readbuffer));
+		}
+
+		for (int C = 0; C < sumPos; C++) {
+			sum += nums[C];
+		}
+
+		sprintf(writebuffer, "%i", sum);
+		write(ptoc[1], writebuffer, sizeof(writebuffer));
+		//printf("Exit parent\n");
 	}
 
 	return 0;
