73be8bb5c94f5e4d8e883a3f58b69a7548c46a3a
Cameron Swinoga <cameron@debainVM64>
2017-01-10 16:46:00 -0500

    Misc

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include "pthread.h"
+
+void bubblesort(int *array) {
+	/*for (unsigned int i = 0; i < myArray->size(); i++)
+	    for (unsigned int j = 0; j < myArray->size()-1; j++)
+	        if (myArray->at(j) < myArray->at(j+1))
+	            swap((*myArray)[j], (*myArray)[j+1]);
+	*/
+}
 
 int main (void) {
+	pthread_exit(NULL); // Wait for all threads to stop execution
 	return 0;
 }
