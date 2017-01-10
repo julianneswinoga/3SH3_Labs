fda751b5b175eccd677cbbf414308e00da308dca
Cameron Swinoga <cameron@DESKTOP-TAFEGDQ.Home>
2017-01-10 17:54:42 -0500

    File reading and debug print mesh

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "pthread.h"
+
+#define DELIM " "
+
+int data[256][256] = { {0 } };
+int height, width;
 
 void bubblesort(int *array) {
 	/*for (unsigned int i = 0; i < myArray->size(); i++)
@@ -9,7 +16,32 @@ void bubblesort(int *array) {
 	*/
 }
 
+void debug_printData() {
+	for (int y = 0; y < height; y++)
+		for (int x = 0; x < width; x++)
+			printf("%s%i\t%s", x == 0 ? "|" : "", data[x][y], x == width - 1 ? "|\n" : "");
+}
+
 int main (void) {
+	FILE *inputFile;
+	char buf[255];
+	int line;
+	inputFile = fopen("input.txt", "r");
+
+	for (line = 0; fgets(buf, sizeof(buf) / sizeof(char), (FILE*)inputFile) != NULL; line++) {
+		char *p = strtok(buf, DELIM);
+
+		for (int i = 0; p != NULL; i++) {
+			data[line][i] = atoi(p);
+			p = strtok(NULL, DELIM);
+			width = i; // Set the data width
+		}
+	}
+
+	fclose(inputFile); // Close input file
+	height = line;
+	debug_printData();
+	///////////////////////////////////////SORT CODE
 	pthread_exit(NULL); // Wait for all threads to stop execution
 	return 0;
 }
