6789f6032d4dfacd0523f0548ab5f1d0a5fcceea
Julianne Swinoga <julianneswinoga@gmail.com>
2017-01-11 10:40:19 -0500

    More setup and compiling options

--- Lab4/alloc.c
+++ Lab4/alloc.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <sys/mman.h>
 
 int main(void) {
+	FILE *resourseFile;
+	void *res;
+	int len = 2048;
+	resourseFile = fopen("res.txt", "w");
+	res = mmap(0, len, PROT_READ | PROT_WRITE, MAP_SHARED, resourseFile, 0);
+	fclose(resourseFile); // Close input file
+
+	if (res == MAP_FAILED) {
+		printf("Memory map failed\n");
+		return 1;
+	}
+
 	return 0;
 }
--- Lab4/prov-rep.c
+++ Lab4/prov-rep.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 
 int main(void) {
+	pid_t childpid;
+
+	if ((childpid = fork()) == -1) {
+		perror("Fork error");
+		exit(1);
+	}
+
+	if (childpid == 0) { // Child process
+		int pageSize;
+
+		pageSize = getpagesize();
+		printf("Page size: %i", pageSize);
+	} else { // Parent process
+	}
+
 	return 0;
 }
