f11f4f1265738a764b6b517d8f6f57fa7b24b27a
Cameron Swinoga <cameron@DESKTOP-TAFEGDQ.Home>
2017-01-10 22:02:58 -0500

    Lab 3 completed

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,47 +1,155 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #include "pthread.h"
 
-#define DELIM " "
+#define FILE_DELIM " "
+#define MAX_THREADS 16
 
 int data[256][256] = { {0 } };
-int height, width;
+int meshSize, threadIds[MAX_THREADS];
+bool threadWorkDone[MAX_THREADS] = {0, };
+pthread_t threads[MAX_THREADS];
+pthread_mutex_t mutex_data, mutex_phaseComplete;
+pthread_cond_t condition_phaseComplete;
 
-void bubblesort(int *array) {
-	/*for (unsigned int i = 0; i < myArray->size(); i++)
-	    for (unsigned int j = 0; j < myArray->size()-1; j++)
-	        if (myArray->at(j) < myArray->at(j+1))
-	            swap((*myArray)[j], (*myArray)[j+1]);
-	*/
+void bubblesort(int array[], int len, bool reverse) {
+	int temp;
+
+	for (unsigned int i = 0; i < len - 1; i++) {
+		for (unsigned int j = 0; j < len - i - 1; j++) {
+			if (array[j + (reverse ? 1 : 0)] > array[j + (reverse ? 0 : 1)]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
 
-void debug_printData() {
-	for (int y = 0; y < height; y++)
-		for (int x = 0; x < width; x++)
-			printf("%s%i\t%s", x == 0 ? "|" : "", data[x][y], x == width - 1 ? "|\n" : "");
+void printData() {
+	for (int y = 0; y < meshSize; y++)
+		for (int x = 0; x < meshSize; x++)
+			printf("%s%i\t%s", x == 0 ? "|" : "", data[y][x], x == meshSize - 1 ? "|\n" : "");
+}
+
+bool allThreadsComplete() {
+	for (int i = 0; i < meshSize; i++) {
+		if (!threadWorkDone[i])
+			return false;
+	}
+
+	return true;
+}
+
+void *meshThreadFunc(void *arg) {
+	printf("#%i Created\n", (int)arg);
+	int arrayCopy[meshSize];
+
+	for (int phase = 0; phase <= ceil(log2(meshSize + 1.0)); phase++) {
+		if (phase % 2 == 0) { // Column sort
+			for (int i = 0; i < meshSize; i++)
+				arrayCopy[i] = data[i][(int)arg]; // Copy column data
+
+			bubblesort(arrayCopy, meshSize, false);
+			pthread_mutex_lock(&mutex_data);
+
+			for (int i = 0; i < meshSize; i++) // Write new column data
+				data[i][(int)arg] = arrayCopy[i];
+
+			pthread_mutex_unlock(&mutex_data);
+
+		} else { // Row sort
+			for (int i = 0; i < meshSize; i++)
+				arrayCopy[i] = data[(int)arg][i]; // Copy row data
+
+			if ((int)arg % 2 == 0) { // Increasing from left to right
+				bubblesort(arrayCopy, meshSize, false);
+
+			} else { // Decreasing from left to right
+				bubblesort(arrayCopy, meshSize, true);
+			}
+
+			pthread_mutex_lock(&mutex_data);
+
+			for (int i = 0; i < meshSize; i++) // Write new row data
+				data[(int)arg][i] = arrayCopy[i];
+
+			pthread_mutex_unlock(&mutex_data);
+		}
+
+		printf("#%i done phase %i\n", (int)arg, phase);
+		threadWorkDone[(int)arg] = true;
+
+		if (!allThreadsComplete()) { // If not all threads are complete, wait
+			pthread_mutex_lock(&mutex_phaseComplete);
+			pthread_cond_wait(&condition_phaseComplete, &mutex_phaseComplete); // Wait for phases to complete
+			pthread_mutex_unlock(&mutex_phaseComplete);
+
+		} else {
+			for (size_t i = 0; i < meshSize; i++)
+				threadWorkDone[i] = false; // Reset thread work
+
+			printf("#%i is the last thread!\n", (int)arg);
+			pthread_cond_broadcast(&condition_phaseComplete); // Broadcast that we can proceed
+		}
+	}
+
+	pthread_exit(NULL);
 }
 
 int main (void) {
 	FILE *inputFile;
 	char buf[255];
 	int line;
+	pthread_attr_t attr;
 	inputFile = fopen("input.txt", "r");
 
 	for (line = 0; fgets(buf, sizeof(buf) / sizeof(char), (FILE*)inputFile) != NULL; line++) {
-		char *p = strtok(buf, DELIM);
+		char *p = strtok(buf, FILE_DELIM);
 
 		for (int i = 0; p != NULL; i++) {
 			data[line][i] = atoi(p);
-			p = strtok(NULL, DELIM);
-			width = i; // Set the data width
+			p = strtok(NULL, FILE_DELIM);
 		}
 	}
 
 	fclose(inputFile); // Close input file
-	height = line;
-	debug_printData();
-	///////////////////////////////////////SORT CODE
+	meshSize = line; // Set the size of the mesh
+
+	if (meshSize > MAX_THREADS) {
+		printf("Error, too many threads needed to sort.");
+		return 1;
+	}
+
+	printData(); // Print unsorted data
+	pthread_mutex_init(&mutex_data, NULL); // Init mutex for data
+	pthread_mutex_init(&mutex_phaseComplete, NULL); // Init mutex for phase complete
+	pthread_cond_init (&condition_phaseComplete, NULL); // Init condition for phase complete
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	for (int i = 0; i < meshSize; i++) { // Create $(meshSize) threads
+		threadIds[i] = i;
+		pthread_create(&threads[i], &attr, meshThreadFunc, (void*)threadIds[i]);
+	}
+
+	pthread_attr_destroy(&attr);
+
+	for (int i = 0; i < meshSize; i++) { // Join all $(meshSize) threads
+		void *status;
+		pthread_join(threads[i], &status);
+
+		if (status != 0) {
+			printf("Thread #%i exited with %i\n", i, status);
+			return 1;
+		}
+	}
+
+	printData(); // Print sorted data
+	pthread_mutex_destroy(&mutex_data); // Destroy mutex
 	pthread_exit(NULL); // Wait for all threads to stop execution
 	return 0;
 }
