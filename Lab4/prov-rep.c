fe5aa14fd91b4b19cb30da0ffdf4e83f6203e3a8
Julianne Swinoga <julianneswinoga@gmail.com>
2017-01-11 12:52:58 -0500

    Added alarm signals to prov-rep.c

--- Lab4/prov-rep.c
+++ Lab4/prov-rep.c
@@ -2,11 +2,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
-int main(void) {
-	pid_t childpid;
+#define ALARM_TIME 2
+
+pid_t childpid;
+
+void signalHandler(int  signo) {
+	if (signo  ==  SIGALRM && childpid == 0) {
+		pageSize = getpagesize();
+		printf("Page size: %i\n", pageSize);
+		alarm(ALARM_TIME); // Set up the next alarm
+	}
+}
+
+void main(void) {
+	if (signal(SIGALRM, signalHandler) ==  SIG_ERR) { //  register  the  signal  handler
+		printf ("Failed to register a signal handler");
+		exit (1);
+	}
 
 	if ((childpid = fork()) == -1) {
 		perror("Fork error");
@@ -15,11 +31,13 @@ int main(void) {
 
 	if (childpid == 0) { // Child process
 		int pageSize;
-		pageSize = getpagesize();
-		printf("Page size: %i", pageSize);
+		alarm(ALARM_TIME); // Set the initial alarm
+		while (1) { sleep(10); }
 
 	} else { // Parent process
+		printf("Parent process\n");
+		while (1) { sleep(10); }
 	}
 
-	return 0;
+	exit(0); // Exit all processes
 }
