96621918b1a3e3f5238de825adb1de821ff79951
Julianne Swinoga <julianneswinoga@gmail.com>
2017-01-26 14:16:09 -0500

    Misc updates

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,11 +1,13 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
-	int ctop[2], ptoc[2];
+	int ctop[2], ptoc[2], status;
 
 	pid_t childpid;
 	pipe(ctop);
@@ -27,23 +29,24 @@ int main(void) {
 		};
 
 		while (strstr(input, "-1") == 0) {
+			printf("Enter an unsigned 8 bit integer: ");
 			scanf("%s", input);
 			write(ctop[1], input, strlen(input) + 1);
 		}
 
 		read(ptoc[0], readbuffer, sizeof(readbuffer));
-		printf("\nSum: %s\r\n", readbuffer);
-	} else {			// Parent process
-		close(ctop[1]); // Close write pipe
-		close(ptoc[0]); // Close read pipe
+		printf("\nSum: %s\n", readbuffer); // Child printing out sum
+	} else {							   // Parent process
+		close(ctop[1]);					   // Close write pipe
+		close(ptoc[0]);					   // Close read pipe
 		char readbuffer[256] = {
 			0,
 		};
 		char writebuffer[256] = {
 			0,
 		};
-		char *endStr;
-		int   nums[256] = {
+		char *  endStr;
+		uint8_t nums[256] = {
 			0,
 		};
 		int			 sum	= 0;
@@ -51,14 +54,15 @@ int main(void) {
 		read(ctop[0], readbuffer, sizeof(readbuffer));
 
 		while (strcmp(readbuffer, "-1") != 0) {
-			nums[sumPos++] = strtol(readbuffer, &endStr, 10);
+			nums[sumPos++] = (uint8_t)strtol(readbuffer, &endStr, 10);
 			read(ctop[0], readbuffer, sizeof(readbuffer));
 		}
 
-		for (int C = 0; C < sumPos; C++) { sum += nums[C]; }
+		for (unsigned int C = 0; C < sumPos; C++) { sum += nums[C]; }
 
-		sprintf(writebuffer, "%i", sum);
+		sprintf(writebuffer, "%i", sum); // Passing sum from parent to child
 		write(ptoc[1], writebuffer, sizeof(writebuffer));
+		wait(&status); // Wait for child process to die
 	}
 
 	return 0;
