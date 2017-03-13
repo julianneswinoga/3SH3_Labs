31076c99b36d157160aa71799741d2c75e63b8d3
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-13 00:06:59 -0500

    Recursive actually working

--- Lab5/main.c
+++ Lab5/main.c
@@ -15,31 +15,27 @@ int printRecursiveStats(char name[]) {
 
 	stat(name, &fileStat);
 
-	printf(S_ISDIR(fileStat.st_mode) ? "d" : "-");
-	printf(fileStat.st_mode & S_IRUSR ? "r" : "-");
-	printf(fileStat.st_mode & S_IWUSR ? "w" : "-");
-	printf(fileStat.st_mode & S_IXUSR ? "x" : "-");
-	printf(fileStat.st_mode & S_IRGRP ? "r" : "-");
-	printf(fileStat.st_mode & S_IWGRP ? "w" : "-");
-	printf(fileStat.st_mode & S_IXGRP ? "x" : "-");
-	printf(fileStat.st_mode & S_IROTH ? "r" : "-");
-	printf(fileStat.st_mode & S_IWOTH ? "w" : "-");
-	printf(fileStat.st_mode & S_IXOTH ? "x" : "-");
-
-	printf(" Links: %lu", fileStat.st_nlink);
-
-	printf(" (%ldB, %lubl)", fileStat.st_size, fileStat.st_blocks);
-	printf(" inode: %lu", fileStat.st_ino);
-
-	printf(" %s", (S_ISLNK(fileStat.st_mode)) ? "S" : "-");
-
 	pwd = getpwuid(fileStat.st_uid);
 	grp = getgrgid(fileStat.st_gid);
-	printf(" %s:%s", pwd != NULL ? pwd->pw_name : "NULL", grp != NULL ? grp->gr_name : "NULL");
 
-	printf(" %s", name);
-
-	printf(" %s", ctime(&fileStat.st_mtime));
+	printf("%c%c%c%c%c%c%c%c%c%c Links: %lu (%ldB, %lubl) inode: %lu, %c, %s:%s \"%s\" %s",
+	       S_ISDIR(fileStat.st_mode) ? 'd' : '-',
+	       fileStat.st_mode & S_IRUSR ? 'r' : '-',
+	       fileStat.st_mode & S_IWUSR ? 'w' : '-',
+	       fileStat.st_mode & S_IXUSR ? 'x' : '-',
+	       fileStat.st_mode & S_IRGRP ? 'r' : '-',
+	       fileStat.st_mode & S_IWGRP ? 'w' : '-',
+	       fileStat.st_mode & S_IXGRP ? 'x' : '-',
+	       fileStat.st_mode & S_IRUSR ? 'r' : '-',
+	       fileStat.st_mode & S_IWOTH ? 'w' : '-',
+	       fileStat.st_mode & S_IXOTH ? 'x' : '-',
+	       fileStat.st_nlink,
+	       fileStat.st_size, fileStat.st_blocks,
+	       fileStat.st_ino,
+	       S_ISLNK(fileStat.st_mode) ? 'S' : '-',
+	       pwd != NULL ? pwd->pw_name : "NULL", grp != NULL ? grp->gr_name : "NULL",
+	       name,
+	       ctime(&fileStat.st_mtime));
 
 	if (S_ISDIR(fileStat.st_mode)) {
 		DIR *          pDir;
@@ -53,12 +49,14 @@ int printRecursiveStats(char name[]) {
 			return 1;
 		}
 
+		char path[256];
+
 		while ((pDirent = readdir(pDir)) != NULL) {
+			strcpy(path, name);
 			if (strcmp(pDirent->d_name, ".") != 0 && strcmp(pDirent->d_name, "..") != 0) {
-				if (printRecursiveStats(pDirent->d_name) != 0) {
-					printf("Error retieving stats for %s\n", pDirent->d_name);
-					return 1;
-				}
+				strcat(path, "/");
+				strcat(path, pDirent->d_name);
+				printRecursiveStats(path);
 			}
 		}
 		printf("Exiting directory %s\n", name);
