98a5c678b89abfea592ed94840963ecd874decca
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-20 15:03:32 -0400

    Style

--- Lab4/alloc.c
+++ Lab4/alloc.c
@@ -5,25 +5,25 @@
 #include <unistd.h>
 
 int main(void) {
-  int   resourseFile, pageSize;
-  char *res;
-  pageSize     = getpagesize();
-  resourseFile = open("res.txt", O_RDWR);
+	int   resourseFile, pageSize;
+	char *res;
+	pageSize     = getpagesize();
+	resourseFile = open("res.txt", O_RDWR);
 
-  if (resourseFile == -1) {
-    printf("File not found\n");
-    return 1;
-  }
+	if (resourseFile == -1) {
+		printf("File not found\n");
+		return 1;
+	}
 
-  res = mmap((uintptr_t)0, pageSize, PROT_READ | PROT_WRITE, MAP_SHARED,
-	     resourseFile, 0);
+	res = mmap((uintptr_t)0, pageSize, PROT_READ | PROT_WRITE, MAP_SHARED,
+	           resourseFile, 0);
 
-  if (res == MAP_FAILED) {
-    printf("Memory map failed\n");
-    return 1;
-  }
+	if (res == MAP_FAILED) {
+		printf("Memory map failed\n");
+		return 1;
+	}
 
-  for (int i = 0; res[i] != 0; i++) printf(">%c\n", res[i]);
+	for (int i = 0; res[i] != 0; i++) printf(">%c\n", res[i]);
 
-  return 0;
+	return 0;
 }
